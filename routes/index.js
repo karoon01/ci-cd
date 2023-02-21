60741bff1b03d441c900261b26907f077c949f5e
karoon01 <xblazeradd@gmail.com>
2023-02-21 15:09:37 +0200

    test workflow on changes

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const { Router: Index } = require('express')
 const rootRouter = Index({ mergeParams: true })
 
 rootRouter.get('/test', (req, res) => res.status(200).send('Success'))
+rootRouter.get('/new', (req, res) => res.status(200).send('new endpoint'))
 
 module.exports = rootRouter
