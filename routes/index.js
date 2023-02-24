86c7abb8a3ee67cad94a66a3bdeaa74cd8dbc893
karoon01 <xblazeradd@gmail.com>
2023-02-24 16:42:21 +0200

    added environments

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const { Router: Index } = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const rootRouter = Index({ mergeParams: true })
 
 rootRouter.get('/test', (req, res) => res.status(200).send('Success'))
 rootRouter.get('/new', (req, res) => res.status(200).send('new endpoint'))
-rootRouter.get('/staging', (req, res) => res.status(200).send('staging env'))
+rootRouter.get('/staging', (req, res) =>
+  res.status(200).send(`Environment: ${process.env.NODE_ENV}`)
+)
 
 module.exports = rootRouter
